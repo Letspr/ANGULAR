ee331657b00617a8e30a5fb5d1ef96053cc0681f
Letspr <lparrondo@gmail.com>
2022-02-04 19:38:37 +0100

    mi primer angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DniComponent } from './components/dni/dni.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"dni",component:DniComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
